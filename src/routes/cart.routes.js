69df0b48c4e30ea6bca5a5410043ff3514d64c32
yogamuz <yogamuz13@gmail.com>
2025-09-01 20:46:12 +0700

    adding middleware to cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const cartController = require('../controllers/cart.controller');
 const { protect } = require('../middlewares/auth.middleware');
 
-// 🟢 Public routes (bisa dipakai guest)
-router.get('/', cartController.getCart);         // Ambil cart
-router.post('/add', cartController.addToCart);   // Tambah item
-router.delete('/remove/:productId', cartController.removeFromCart); 
-router.put('/update/:productId', cartController.updateCartItem);
+router.use(protect);
 
 // 🔒 Protected routes (khusus user login)
-router.use(protect);
+router.delete('/remove/:productId', cartController.removeFromCart); 
+router.put('/update/:productId', cartController.updateCartItem);
+router.get('/', cartController.getCart);         // Ambil cart
+router.post('/add', cartController.addToCart);   // Tambah item
 
 router.get('/count', cartController.getCartCount);
 router.delete('/clear', cartController.clearCart);
